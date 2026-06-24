873d856c9b1fbe4a5eef2bb4571450b4a109e7bb
Pranith <reddypranith80@gmail.com>
2026-06-24 21:22:39 +0530

    two sum hashmap sol

--- Arrays/1.Two_Sum.java
+++ Arrays/1.Two_Sum.java
@@ -13,3 +13,22 @@ class Solution {
         return val;
     }
 }
+
+// hashmap solution
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int val[] = new int[2];
+        HashMap<Integer,Integer>hm = new HashMap<>();
+            for(int i=0;i<n;i++){
+                int comp = target - nums[i];
+                if(hm.containsKey(comp)){
+                    return new int[]{hm.get(comp),i};
+                }
+                hm.put(nums[i],i);
+            }
+            return new int[]{};
+    }
+}
+
