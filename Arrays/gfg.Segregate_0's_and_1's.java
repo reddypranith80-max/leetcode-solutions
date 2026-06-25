8e16868f6e16a04cf01c7d087d4ca5cfa8288adc
Pranith <reddypranith80@gmail.com>
2026-06-26 00:30:37 +0530

    gfg segregate 0 and 1 using two pointer

--- Arrays/gfg.Segregate_0's_and_1's.java
+++ Arrays/gfg.Segregate_0's_and_1's.java
@@ -23,3 +23,39 @@ class Solution {
         
     }
 }
+
+
+
+// using two pointer approach
+class Solution {
+    void segregate0and1(int[] arr) {
+        // code here
+        int n = arr.length;
+        int newarr[] = new int[n];
+        int i=0;
+        int j=n-1;
+        
+       while(i<j){
+           if(arr[i] == 0 && arr[j] == 0){
+               i++;
+           }
+           else if(arr[i] == 1 && arr[j] == 1){
+               j--;
+           }
+           else if(arr[i] == 0 && arr[j] == 1){
+               i++;
+               j--;
+           }
+           // if(arr[i] == 1 && arr[j] == 0)
+           else{
+               int temp = arr[i];
+               arr[i] = arr[j];
+               arr[j] = temp;
+              i++;
+              j--;
+           }
+       }
+       
+        
+    }
+}
