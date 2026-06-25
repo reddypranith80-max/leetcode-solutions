b70e5f294d61b7871a887603b67a1cbac851ec11
Pranith <reddypranith80@gmail.com>
2026-06-26 01:11:04 +0530

    sorted square sol using 2 pointer approach

--- Arrays/977.Squares_of_sorted_array.java
+++ Arrays/977.Squares_of_sorted_array.java
@@ -9,3 +9,24 @@ class Solution {
         return newarr;
     }
 }
+
+// using two pointer approach
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] res = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                res[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                res[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return res;        
+    }
+}
