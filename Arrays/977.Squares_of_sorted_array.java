980cb852785f6f6eb5ae41f96fb5861aab30fe77
Pranith <reddypranith80@gmail.com>
2026-06-26 16:02:12 +0530

    squares of sorted arrays referencing  merge two sorted arrays using two pointers

--- Arrays/977.Squares_of_sorted_array.java
+++ Arrays/977.Squares_of_sorted_array.java
@@ -30,3 +30,73 @@ class Solution {
         return res;        
     }
 }
+
+
+// using the two pointer approach with a while loop using merge two sorted arrays approach
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        List<Integer>neg = new ArrayList<>();
+        List<Integer>pos = new ArrayList<>();
+
+        int n = nums.length;
+        // using two list to add neg in one list and pos in one list 
+
+        for(int num : nums){
+            if(num < 0){
+                neg.add(num);
+            }else{
+                pos.add(num);
+            }
+        }
+        // multiply neg arr and reverse sort it 
+
+        for(int i=0;i<neg.size();i++){
+            neg.set(i,neg.get(i) * neg.get(i) );
+        }
+        Collections.reverse(neg);
+        // multiply thr pos array auto sorted
+
+        for(int i=0;i<pos.size();i++){
+            pos.set(i,pos.get(i) * pos.get(i) );
+        }
+
+
+        if(neg.size() == 0) { // no negative element only pos 
+            int ans[] = new int[pos.size()];
+            for(int i=0;i<pos.size();i++){
+                ans[i] = pos.get(i);
+            }
+            return ans;
+        }
+
+        if(pos.size() == 0){
+            int ans[] = new int[neg.size()];
+            for(int i=0;i<neg.size();i++){
+                ans[i] = neg.get(i);
+            }
+            return ans;
+        }
+
+        int i=0;
+        int j=0;
+        int idx=0;
+        // using two pointer sorted arrays logic 
+        int res[] = new int[pos.size() + neg.size()];
+        while(i<neg.size() && j<pos.size()){
+            if(neg.get(i) <= pos.get(j)){
+                res[idx++] = neg.get(i++);
+            }else{
+                res[idx++] = pos.get(j++);
+            }
+        }
+        while(i < neg.size()){
+               res[idx++] = neg.get(i++);
+        }
+
+        while(j < pos.size()){
+            res[idx++] = pos.get(j++);
+        }
+        return res;
+    }
+}
